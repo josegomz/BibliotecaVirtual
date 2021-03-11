8b9496c3273e1372ee01776d22856307be67ee85
jose gomez <josegomz.sa17@gmail.com>
2021-03-11 09:16:26 -0600

    PRimer commit

---
+++ src/main/java/spring/mx/BibliotecaVirtualApplication.java
@@ -0,0 +1,13 @@
+package spring.mx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaVirtualApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaVirtualApplication.class, args);
+	}
+
+}
